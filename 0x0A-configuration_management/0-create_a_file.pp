6bef5999008995e0383415308ed2a7458ec7a14d
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-22 12:36:07 -0800

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Using puppet to create a file with these specifications
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
