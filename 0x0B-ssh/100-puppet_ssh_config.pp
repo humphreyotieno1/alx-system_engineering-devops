0f10edd7a451593e4a00665006cdd06a1e7d37a1
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-04 11:34:41 -0800

    Adding ssh key

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 # Connecting without a password
 
-file { '/etc/ssh/ssh_config':
-  ensure => present,
-}
+include stdlib
 
 file_line { 'Turn off passwd auth':
+  ensure  => present,
   path	  => '/etc/ssh/ssh_config',
   line	  => 'PasswordAuthentication no',
   match	  => '^#PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
+  ensure  => present,
   path	  => '/etc/ssh/ssh_config',
   line	  => 'IdentifyFile ~/.ssh/school',
   match	  => '^IdentifyFile',
