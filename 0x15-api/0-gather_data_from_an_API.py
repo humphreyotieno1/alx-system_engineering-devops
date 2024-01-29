2a7ed90f77070817b1b00121dfb9b05c711bf144
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-01-29 21:20:11 +0000

    Fetch data from API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""Return the to-do list information for employee ID."""
+"""Return a to do list information for employee ID."""
 import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(employee_id)).json()
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+if __name__ == "__main__":
+    baseUrl = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(baseUrl + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(baseUrl + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
-
-if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
