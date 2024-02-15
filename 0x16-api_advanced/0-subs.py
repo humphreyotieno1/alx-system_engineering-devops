74afea229d82e6ed9e76b706bce30b4bb1dbfb17
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-02-15 11:49:52 +0000

    Query the subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""Query a reddit API and return number of subscribers"""
+"""
+Query Reddit API for number of subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+        return number of subscribers for a given subreddit
+        return 0 if invalid subreddit given
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'My User Agent'} 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            return 0
-    else:
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    r = requests.get(url, headers=headers).json()
+    subscribers = r.get('data', {}).get('subscribers')
+    if not subscribers:
         return 0
+    return subscribers
